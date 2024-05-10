a83804e6d18d6fd24ad2b1493f16687696ad6c37
Lnola <nola.luka@gmail.com>
2024-05-11 00:43:45 +0200

    :seedling: Create a trailer seeder

--- server/src/shared/database/seeds/database.seeder.ts
+++ server/src/shared/database/seeds/database.seeder.ts
@@ -3,10 +3,11 @@ import { Seeder } from '@mikro-orm/seeder';
 import { CompanyLocationSeeder } from './company-location.seeder';
 import { CompanySeeder } from './company.seeder';
 import { DriverSeeder } from './driver.seeder';
+import { TrailerSeeder } from './trailer.seeder';
 import { TruckSeeder } from './truck.seeder';
 
 export class DatabaseSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    return this.call(em, [CompanySeeder, CompanyLocationSeeder, DriverSeeder, TruckSeeder]);
+    return this.call(em, [CompanySeeder, CompanyLocationSeeder, DriverSeeder, TruckSeeder, TrailerSeeder]);
   }
 }
---
+++ server/src/shared/database/seeds/trailer.seeder.ts
@@ -0,0 +1,10 @@
+import { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import Trailer from 'trailer/trailer.entity';
+
+export class TrailerSeeder extends Seeder {
+  async run(em: EntityManager): Promise<void> {
+    const trailers = [new Trailer('ZG-1235-RE', '2018', 20, 12.5), new Trailer('DA-432-E', '2017', 25, 14.0)];
+    await em.persistAndFlush(trailers);
+  }
+}
