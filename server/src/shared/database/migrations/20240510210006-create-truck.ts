d8a2cebf7af6bf2a2d7c66b0ff1880254e8d1f37
Lnola <nola.luka@gmail.com>
2024-05-10 23:21:18 +0200

    :sparkles: Add a CreateTruck migration

---
+++ server/src/shared/database/migrations/20240510210006-create-truck.ts
@@ -0,0 +1,25 @@
+import { Migration } from '@mikro-orm/migrations';
+
+const TABLE_NAME = 'truck';
+
+export class CreateTruck extends Migration {
+  async up(): Promise<void> {
+    const knex = this.getKnex();
+
+    const createTruckTable = knex.schema.createTable(TABLE_NAME, table => {
+      table.increments('id');
+      table.string('registration').notNullable().unique();
+      table.string('productionYear');
+      table.decimal('tankCapacity').notNullable();
+      table.integer('horsepower');
+    });
+
+    this.addSql(createTruckTable.toQuery());
+  }
+
+  async down(): Promise<void> {
+    const knex = this.getKnex();
+
+    this.addSql(knex.schema.dropTable(TABLE_NAME).toQuery());
+  }
+}
