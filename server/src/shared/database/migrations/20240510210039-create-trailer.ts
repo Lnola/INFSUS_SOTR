041d513c8cab6ae4e6cf6a40d0ccbd53a3e11c2e
Lnola <nola.luka@gmail.com>
2024-05-10 23:21:26 +0200

    :sparkles: Add a CreateTrailer migration

---
+++ server/src/shared/database/migrations/20240510210039-create-trailer.ts
@@ -0,0 +1,25 @@
+import { Migration } from '@mikro-orm/migrations';
+
+const TABLE_NAME = 'trailer';
+
+export class CreateTrailer extends Migration {
+  async up(): Promise<void> {
+    const knex = this.getKnex();
+
+    const createTrailerTable = knex.schema.createTable(TABLE_NAME, table => {
+      table.increments('id');
+      table.string('registration').notNullable().unique();
+      table.string('productionYear');
+      table.integer('palletCapacity').notNullable();
+      table.decimal('length');
+    });
+
+    this.addSql(createTrailerTable.toQuery());
+  }
+
+  async down(): Promise<void> {
+    const knex = this.getKnex();
+
+    this.addSql(knex.schema.dropTable(TABLE_NAME).toQuery());
+  }
+}
