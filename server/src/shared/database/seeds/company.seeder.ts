771c0796a114ecf5dd40a58e8835c10ead023b92
Lnola <nola.luka@gmail.com>
2024-05-11 00:21:26 +0200

    🌱 Create a company seeder

---
+++ server/src/shared/database/seeds/company.seeder.ts
@@ -0,0 +1,10 @@
+import { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import Company from 'company/company.entity';
+
+export class CompanySeeder extends Seeder {
+  async run(em: EntityManager): Promise<void> {
+    const companies = [new Company('12345678901', 'Konzum plus d.o.o.'), new Company('98765432109', 'Instar d.o.o.')];
+    await em.persistAndFlush(companies);
+  }
+}
