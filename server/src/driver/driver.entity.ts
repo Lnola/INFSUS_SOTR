8ea3108e24cca4e9ca6418a4d153ea1b0b3e502e
Lnola <nola.luka@gmail.com>
2024-05-10 20:17:57 +0200

    :sparkles: Create the driver entity

---
+++ server/src/driver/driver.entity.ts
@@ -0,0 +1,38 @@
+import { Entity, Property, Unique } from '@mikro-orm/core';
+import BaseEntity from 'shared/database/base.entity';
+
+@Entity({ tableName: 'Driver' })
+class Driver extends BaseEntity {
+  @Property()
+  firstName: string;
+
+  @Property()
+  lastName: string;
+
+  @Property()
+  @Unique()
+  contactNumber: string;
+
+  @Property()
+  employmentStartDate: Date;
+
+  @Property({ nullable: true })
+  employmentEndDate?: Date;
+
+  constructor(
+    firstName: string,
+    lastName: string,
+    contactNumber: string,
+    employmentStartDate: Date,
+    employmentEndDate?: Date,
+  ) {
+    super();
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.contactNumber = contactNumber;
+    this.employmentStartDate = employmentStartDate;
+    this.employmentEndDate = employmentEndDate;
+  }
+}
+
+export default Driver;
