cb2bee1207935d46d142f10604753544cc397c4e
Lnola <nola.luka@gmail.com>
2024-05-10 20:18:14 +0200

    :sparkles: Add the create driver migration

---
+++ server/src/shared/database/migrations/20240510180846-create-driver.ts
@@ -0,0 +1,26 @@
+import { Migration } from '@mikro-orm/migrations';
+
+const TABLE_NAME = 'driver';
+
+export class CreateDriverTable extends Migration {
+  async up(): Promise<void> {
+    const knex = this.getKnex();
+
+    const createDriverTable = knex.schema.createTable(TABLE_NAME, table => {
+      table.increments('id');
+      table.string('firstName').notNullable();
+      table.string('lastName').notNullable();
+      table.string('contactNumber').notNullable().unique();
+      table.date('employmentStartDate').notNullable();
+      table.date('employmentEndDate').nullable();
+    });
+
+    this.addSql(createDriverTable.toQuery());
+  }
+
+  async down(): Promise<void> {
+    const knex = this.getKnex();
+
+    this.addSql(knex.schema.dropTable(TABLE_NAME).toQuery());
+  }
+}
