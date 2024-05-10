884f7cf4b75f9cc0710726572aeba22eb776eaf5
Lnola <nola.luka@gmail.com>
2024-05-11 00:54:15 +0200

    :bug: Fix the logistics operation seeder issues

--- server/src/shared/database/seeds/logistics-operation.seeder.ts
+++ server/src/shared/database/seeds/logistics-operation.seeder.ts
@@ -9,15 +9,15 @@ export class LogisticsOperationSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
     const operationTypes = [new LogisticsOperationType('Utovar'), new LogisticsOperationType('Istovar')];
 
-    const locations = this.getLocations(em);
-    const orders = this.getOrders(em);
+    const locations = await this.getLocations(em);
+    const orders = await this.getOrders(em);
     const operations = [
-      new LogisticsOperation(new Date('2023-10-01 14:30:00'), true, null, locations[0], locations[0], orders[0]),
-      new LogisticsOperation(new Date('2023-10-02 14:30:00'), false, null, locations[1], locations[1], orders[1]),
-      new LogisticsOperation(new Date('2023-10-03 14:30:00'), true, null, locations[0], locations[0], orders[2]),
-      new LogisticsOperation(new Date('2023-10-04 14:30:00'), false, null, locations[1], locations[1], orders[3]),
-      new LogisticsOperation(new Date('2023-10-05 14:30:00'), true, null, locations[0], locations[0], orders[4]),
-      new LogisticsOperation(new Date('2023-10-06 14:30:00'), false, null, locations[1], locations[1], orders[5]),
+      new LogisticsOperation(new Date('2023-10-01 14:30:00'), true, null, locations[0], operationTypes[0], orders[0]),
+      new LogisticsOperation(new Date('2023-10-02 14:30:00'), false, null, locations[1], operationTypes[1], orders[1]),
+      new LogisticsOperation(new Date('2023-10-03 14:30:00'), true, null, locations[0], operationTypes[0], orders[2]),
+      new LogisticsOperation(new Date('2023-10-04 14:30:00'), false, null, locations[1], operationTypes[1], orders[3]),
+      new LogisticsOperation(new Date('2023-10-05 14:30:00'), true, null, locations[0], operationTypes[0], orders[4]),
+      new LogisticsOperation(new Date('2023-10-06 14:30:00'), false, null, locations[1], operationTypes[1], orders[5]),
     ];
 
     await em.persistAndFlush([...operationTypes, ...operations]);
