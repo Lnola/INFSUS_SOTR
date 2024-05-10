914eae3ba6900974d336769749e4a7acc541419b
Lnola <nola.luka@gmail.com>
2024-05-10 19:30:29 +0200

    :bug: Move the config to the registerAs method to keep the environment variables

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -4,26 +4,28 @@ import { SeedManager } from '@mikro-orm/seeder';
 import { registerAs } from '@nestjs/config';
 import { dash } from 'radash';
 
-const config = defineConfig({
-  port: +process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  loadStrategy: LoadStrategy.JOINED,
-  debug: process.env.NODE_ENV !== 'production',
-  migrations: {
-    path: `${process.cwd()}/dist/shared/database/migrations`,
-    pathTs: `${process.cwd()}/src/shared/database/migrations`,
-    glob: '!(*.d).{js,ts}',
-    fileName: (timestamp: string) => `${timestamp}-new-migration`,
-  },
-  seeder: {
-    path: `${process.cwd()}/dist/shared/database/seeds`,
-    pathTs: `${process.cwd()}/src/shared/database/seeds`,
-    fileName: (className: string) => dash(className),
-  },
-  extensions: [Migrator, SeedManager],
-});
+export default registerAs('database', () => {
+  const config = defineConfig({
+    port: +process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    dbName: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    loadStrategy: LoadStrategy.JOINED,
+    debug: process.env.NODE_ENV !== 'production',
+    migrations: {
+      path: `${process.cwd()}/dist/shared/database/migrations`,
+      pathTs: `${process.cwd()}/src/shared/database/migrations`,
+      glob: '!(*.d).{js,ts}',
+      fileName: (timestamp: string) => `${timestamp}-new-migration`,
+    },
+    seeder: {
+      path: `${process.cwd()}/dist/shared/database/seeds`,
+      pathTs: `${process.cwd()}/src/shared/database/seeds`,
+      fileName: (className: string) => dash(className),
+    },
+    extensions: [Migrator, SeedManager],
+  });
 
-export default registerAs('database', () => config);
+  return config;
+});
