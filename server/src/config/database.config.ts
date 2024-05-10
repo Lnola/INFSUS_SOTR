2eec055bd04387fcb5fe832d01a043e5cd2d5add
Lnola <nola.luka@gmail.com>
2024-05-10 19:03:33 +0200

    :wrench: Create a database config

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -1,22 +1,29 @@
-import { ConfigModule, registerAs } from '@nestjs/config';
+import { Migrator } from '@mikro-orm/migrations';
+import { defineConfig, LoadStrategy } from '@mikro-orm/postgresql';
+import { SeedManager } from '@mikro-orm/seeder';
+import { registerAs } from '@nestjs/config';
+import { dash } from 'radash';
 
-const databaseConfig = registerAs('database', () => ({
-  port: process.env.DB_PORT,
+const config = defineConfig({
+  port: +process.env.DB_PORT,
   host: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  type: 'postgresql',
+  loadStrategy: LoadStrategy.JOINED,
   debug: process.env.NODE_ENV !== 'production',
   migrations: {
-    path: `${process.cwd()}/src/shared/database/migrations`,
-    disableForeignKeys: false,
-    pattern: /^\d+[\w-]+\.ts$/,
+    path: `${process.cwd()}/dist/shared/database/migrations`,
+    pathTs: `${process.cwd()}/src/shared/database/migrations`,
+    glob: '!(*.d).{js,ts}',
     fileName: (timestamp: string) => `${timestamp}-new-migration`,
   },
-}));
-export default databaseConfig;
-
-export const DbConfigModule = ConfigModule.forRoot({
-  load: [databaseConfig],
+  seeder: {
+    path: `${process.cwd()}/dist/shared/database/seeds`,
+    pathTs: `${process.cwd()}/src/shared/database/seeds`,
+    fileName: (className: string) => dash(className),
+  },
+  extensions: [Migrator, SeedManager],
 });
+
+export default registerAs('database', () => config);
