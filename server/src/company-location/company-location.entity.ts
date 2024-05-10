8b76ea22aa6b469bb8d8672866f0ea55e79638af
Lnola <nola.luka@gmail.com>
2024-05-10 22:38:50 +0200

    :sparkles: Add one to many relation of logistics operations to company location

--- server/src/company-location/company-location.entity.ts
+++ server/src/company-location/company-location.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, Property, ManyToOne, Unique } from '@mikro-orm/core';
+import { Entity, Property, ManyToOne, Unique, OneToMany, Collection } from '@mikro-orm/core';
 import Company from 'company/company.entity';
+import LogisticsOperation from 'logistics-operation/logistics-operation.entity';
 import BaseEntity from 'shared/database/base.entity';
 
 @Entity({ tableName: 'company_location' })
@@ -22,6 +23,15 @@ class CompanyLocation extends BaseEntity {
 
   @ManyToOne(() => Company)
   company: Company;
+
+  // TODO: decide if it needs to be private and eager
+  @OneToMany({
+    entity: () => LogisticsOperation,
+    mappedBy: (it: LogisticsOperation) => it.companyLocation,
+    orphanRemoval: true,
+    eager: false,
+  })
+  _logisticsOperations = new Collection<LogisticsOperation>(this);
 
   constructor(street: string, zip: string, city: string, country: string, name: string, company: Company) {
     super();
