2461ff125fd1760523bd3bc5a352f5f1d2432bd1
Lnola <nola.luka@gmail.com>
2024-05-10 23:21:09 +0200

    :sparkles: Add a CreateCompanyLocation migration

---
+++ server/src/shared/database/migrations/20240510182148-create-company-location.ts
@@ -0,0 +1,28 @@
+import { Migration } from '@mikro-orm/migrations';
+
+const TABLE_NAME = 'company_location';
+
+export class CreateCompanyLocation extends Migration {
+  async up(): Promise<void> {
+    const knex = this.getKnex();
+
+    const createCompanyLocationTable = knex.schema.createTable(TABLE_NAME, table => {
+      table.increments('id');
+      table.string('street').notNullable();
+      table.string('zip').notNullable();
+      table.string('city').notNullable();
+      table.string('country').notNullable();
+      table.string('name').notNullable().unique();
+      table.integer('companyId').notNullable();
+      table.foreign('companyId').references('id').inTable('company');
+    });
+
+    this.addSql(createCompanyLocationTable.toQuery());
+  }
+
+  async down(): Promise<void> {
+    const knex = this.getKnex();
+
+    this.addSql(knex.schema.dropTable(TABLE_NAME).toQuery());
+  }
+}
