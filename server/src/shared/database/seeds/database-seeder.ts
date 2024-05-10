2cc9318c3c58e64ca3a7be7ab23cad40b3d1e08f
Lnola <nola.luka@gmail.com>
2024-05-11 00:22:09 +0200

    :seedling: Create a database seeder

---
+++ server/src/shared/database/seeds/database-seeder.ts
@@ -0,0 +1,10 @@
+import type { EntityManager } from '@mikro-orm/postgresql';
+import { Seeder } from '@mikro-orm/seeder';
+import { CompanyLocationSeeder } from './company-location.seeder';
+import { CompanySeeder } from './company.seeder';
+
+export class DatabaseSeeder extends Seeder {
+  async run(em: EntityManager): Promise<void> {
+    return this.call(em, [CompanySeeder, CompanyLocationSeeder]);
+  }
+}
