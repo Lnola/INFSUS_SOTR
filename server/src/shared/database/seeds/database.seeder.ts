fa4f9d935ee3c98831c3e94832ed14ad8741a6d4
Lnola <nola.luka@gmail.com>
2024-05-11 00:24:29 +0200

    :seedling: Create a driver seeder

--- server/src/shared/database/seeds/database.seeder.ts
+++ server/src/shared/database/seeds/database.seeder.ts
@@ -2,9 +2,10 @@ import type { EntityManager } from '@mikro-orm/postgresql';
 import { Seeder } from '@mikro-orm/seeder';
 import { CompanyLocationSeeder } from './company-location.seeder';
 import { CompanySeeder } from './company.seeder';
+import { DriverSeeder } from './driver.seeder';
 
 export class DatabaseSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    return this.call(em, [CompanySeeder, CompanyLocationSeeder]);
+    return this.call(em, [CompanySeeder, CompanyLocationSeeder, DriverSeeder]);
   }
 }
---
+++ server/src/shared/database/seeds/driver.seeder.ts
@@ -0,0 +1,13 @@
+import { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import Driver from 'driver/driver.entity';
+
+export class DriverSeeder extends Seeder {
+  async run(em: EntityManager): Promise<void> {
+    const drivers = [
+      new Driver('John', 'Doe', '123-456-7890', new Date('2012-01-15'), null),
+      new Driver('Jane', 'Smith', '987-654-3210', new Date('2019-05-20'), new Date('2022-12-31')),
+    ];
+    await em.persistAndFlush(drivers);
+  }
+}
