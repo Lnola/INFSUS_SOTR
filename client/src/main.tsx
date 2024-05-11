85ae12cafe997d62146ee2f20ba9254696ba77e2
Lnola <nola.luka@gmail.com>
2024-05-11 02:49:20 +0200

    :fire: Remove the chakra provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
@@ -6,8 +5,6 @@ import { router } from './router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
