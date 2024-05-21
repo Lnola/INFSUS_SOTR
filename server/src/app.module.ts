fbb5696c6d6476caed36007c1878fc9fa9456551
Lnola <nola.luka@gmail.com>
2024-05-21 23:29:18 +0200

    :sparkles: Register the driver module to the app

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import databaseConfig from 'config/database.config';
+import { DriverModule } from 'driver/driver.module';
 import { HealthController } from 'health.controller';
 import { DatabaseModule } from 'shared/database/database.module';
 import { CompanyLocationModule } from './company-location/company-location.module';
@@ -11,7 +12,7 @@ const GlobalConfigModule = ConfigModule.forRoot({
 });
 
 @Module({
-  imports: [GlobalConfigModule, DatabaseModule, CompanyLocationModule],
+  imports: [GlobalConfigModule, DatabaseModule, CompanyLocationModule, DriverModule],
   controllers: [HealthController],
 })
 export class AppModule {}
