b1d0739253c546bcbf13173d8b8335af89679cfc
Lnola <nola.luka@gmail.com>
2024-05-21 23:28:47 +0200

    :sparkles: Create a basic driver controller

---
+++ server/src/driver/driver.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { DriverService } from './driver.service';
+
+@Controller('drivers')
+export class DriverController {
+  constructor(private readonly driverService: DriverService) {}
+
+  @Get()
+  findAll() {
+    return this.driverService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.driverService.findOne(+id);
+  }
+}
--- server/src/driver/driver.module.ts
+++ server/src/driver/driver.module.ts
@@ -1,10 +1,12 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { DriverController } from './driver.controller';
 import { DriverService } from './driver.service';
 import Driver from './entities/driver.entity';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Driver])],
   providers: [DriverService],
+  controllers: [DriverController],
 })
 export class DriverModule {}
