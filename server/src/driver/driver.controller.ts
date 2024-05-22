f6ac444a2cbfd505d224182db5ff8e9b994adb28
Lnola <nola.luka@gmail.com>
2024-05-22 02:14:45 +0200

    🧑‍💻 Create pagination parameters decorator

--- server/src/driver/driver.controller.ts
+++ server/src/driver/driver.controller.ts
@@ -1,12 +1,41 @@
+import { BadRequestException, createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Controller, Get, Param } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { IsNumberString, IsOptional } from 'class-validator';
+import { validateOrReject } from 'class-validator';
 import { DriverService } from './driver.service';
+
+class PaginationParams {
+  @IsNumberString()
+  @IsOptional()
+  page?: number;
+
+  @IsNumberString()
+  @IsOptional()
+  pageSize?: number;
+}
+
+export const Pagination = createParamDecorator(async (_: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  const pagination = plainToClass(PaginationParams, request.query);
+
+  try {
+    await validateOrReject(pagination);
+  } catch (error) {
+    throw new BadRequestException(error);
+  }
+
+  const { page, pageSize } = pagination;
+  return { page, pageSize };
+});
 
 @Controller('drivers')
 export class DriverController {
   constructor(private readonly driverService: DriverService) {}
 
   @Get()
-  findAll() {
+  async findAll(@Pagination() pagination: PaginationParams) {
+    console.log('Pagination:', pagination);
     return this.driverService.findAll();
   }
 
