24320ebae71f4a38829909a5f4a73c1496c0c9c1
Mario Hosnjak <mario.hosnjak009@gmail.com>
2024-05-23 02:29:24 +0200

    Show dummy data with @mui DataGrid

--- client/src/pages/Vehicle/truck.tsx
+++ client/src/pages/Vehicle/truck.tsx
@@ -1,3 +1,5 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
 type Truck = {
   id: number;
   registration: string;
@@ -5,6 +7,14 @@ type Truck = {
   reservoarCapacity: number;
   horsepower: number;
 };
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'registration', headerName: 'Registration', width: 130 },
+  { field: 'makeYear', headerName: 'Make year', type: 'number', width: 80 },
+  { field: 'reservoarCapacity', headerName: 'Reservoar', type: 'number', width: 90,},
+  { field: 'horsepower', headerName: 'Horsepower', type: 'number', width: 90,},
+];
 
 const DUMMY_TRUCKS: Truck[] = [
   { id: 1, registration: 'VŽ-393-OL', makeYear: '2019', reservoarCapacity: 1420, horsepower: 480 },
@@ -17,11 +27,18 @@ const DUMMY_TRUCKS: Truck[] = [
 
 const TruckList = () => {
   return (
-    <div>
-      {DUMMY_TRUCKS.map(truck => (
-        <p key={truck.id}>Id: {truck.id}, Reg: {truck.registration}, Year: {truck.makeYear},
-        Reservoar: {truck.reservoarCapacity}L, HP: {truck.horsepower}</p>
-      ))}
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={DUMMY_TRUCKS}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
     </div>
   );
 };
