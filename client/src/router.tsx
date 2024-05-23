5af5eb8bb3b771c53327011d3b4cb84ede58df1b
Mario Hosnjak <mario.hosnjak009@gmail.com>
2024-05-23 02:18:26 +0200

    Add /trucks router

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
+import TruckList from './pages/Vehicle/truck';
 import DriverList from '@/pages/Driver';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <DriverList />,
+  },
+  {
+    path: '/trucks',
+    element: <TruckList />,
   },
 ]);
