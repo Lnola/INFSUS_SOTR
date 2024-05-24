3a350fa9e5b6ce038f9224f777403a6955fa4dbd
Lnola <nola.luka@gmail.com>
2024-05-24 20:05:38 +0200

    :sparkles: Update useFetch to allow for count return value

--- client/src/hooks/useFetch.ts
+++ client/src/hooks/useFetch.ts
@@ -19,61 +19,74 @@ type ConditionalProps = GetProps | PostProps;
 type Props = BaseProps & ConditionalProps;
 export type FetchReturn<T> = {
   data: T | undefined;
+  count: number | undefined;
   isLoading: boolean;
-  refetch: () => void;
+  refetch: (paginationParams?: PaginationParams) => void;
   error: Error | null;
 };
 
 function useFetch<T>({ path, method, data, query }: Props): FetchReturn<T> {
   const [result, setResult] = useState<T>();
+  const [count, setCount] = useState<number | undefined>();
   const [isLoading, setIsLoading] = useState(true);
   const [internalLoading, setInternalLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
 
-  const fetchData = useCallback(async () => {
-    const config = { headers: { 'Content-Type': 'application/json' } };
-    const request = axios.create(config);
+  const fetchData = useCallback(
+    async (paginationParams?: PaginationParams) => {
+      const config = { headers: { 'Content-Type': 'application/json' } };
+      const request = axios.create(config);
 
-    const lowercaseMethod = method.toLowerCase() as MethodLowercase;
-    return await request[lowercaseMethod](path, { params: { ...data, ...query } });
+      console.log({ ...data, ...query, ...paginationParams });
+      const lowercaseMethod = method.toLowerCase() as MethodLowercase;
+      return await request[lowercaseMethod](path, { params: { ...data, ...query, ...paginationParams } });
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data, method, path]);
+    [data, method, path],
+  );
 
-  const fetchAndFormat = useCallback(async () => {
-    try {
-      const response = await fetchData();
-      const data = await response.data;
-      setResult(data);
-      setError(null);
-    } catch (error) {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const axiosError = error as any;
-      setError(axiosError.response?.data || axiosError);
-    } finally {
-      setIsLoading(false);
-      setInternalLoading(false);
-    }
-  }, [fetchData]);
+  const fetchAndFormat = useCallback(
+    async (paginationParams?: PaginationParams) => {
+      try {
+        const response = await fetchData(paginationParams);
+        const data = await response.data;
+        setCount(data.count);
+        setResult(data.items);
+        setError(null);
+      } catch (error) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const axiosError = error as any;
+        setError(axiosError.response?.data || axiosError);
+      } finally {
+        setIsLoading(false);
+        setInternalLoading(false);
+      }
+    },
+    [fetchData],
+  );
 
-  const fetchEnsureSingleActive = useCallback(async () => {
-    if (internalLoading) {
-      return;
-    }
-    setInternalLoading(true);
-    setIsLoading(true);
-    await fetchAndFormat();
+  const fetchEnsureSingleActive = useCallback(
+    async (paginationParams?: PaginationParams) => {
+      if (internalLoading) {
+        return;
+      }
+      setInternalLoading(true);
+      setIsLoading(true);
+      await fetchAndFormat(paginationParams);
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [fetchAndFormat]);
+    [fetchAndFormat],
+  );
 
   useEffect(() => {
     fetchEnsureSingleActive();
   }, [fetchEnsureSingleActive]);
 
-  const refetch = () => {
-    fetchEnsureSingleActive();
+  const refetch = (paginationParams?: PaginationParams) => {
+    fetchEnsureSingleActive(paginationParams);
   };
 
-  return { data: result, isLoading, error, refetch };
+  return { data: result, count, isLoading, error, refetch };
 }
 
 export default useFetch;
