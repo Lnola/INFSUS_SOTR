ce1d7f2d6aa5c46c811e189c737b7b6614c06ed9
Mario Hosnjak <mario.hosnjak009@gmail.com>
2024-05-26 00:58:53 +0200

    Added sorted pagination by ID

--- server/src/truck/truck.service.ts
+++ server/src/truck/truck.service.ts
@@ -18,6 +18,7 @@ export class TruckService {
     const paginationOptions = {
       offset: page * pageSize,
       limit: pageSize,
+      orderBy: { id: 'ASC' },
     };
     const [items, count] = await this.truckRepository.findAndCount({}, paginationOptions);
     return { items, count };
