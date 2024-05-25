0ced45d62ffd0c361b1fa5708cf980172bdfe07e
Lnola <nola.luka@gmail.com>
2024-05-26 00:51:07 +0200

    :sparkles: Create the update url

--- client/src/api/driver.ts
+++ client/src/api/driver.ts
@@ -4,9 +4,10 @@ import request from './request';
 const base = urljoin('/api/drivers');
 const get = base;
 const create = base;
+const update = (id: number) => urljoin(base, id.toString());
 const remove = (id: number) => urljoin(base, id.toString());
 
-export default { get, create, remove };
+export default { get, create, update, remove };
 
 export const removeDriver = async (id: number) => {
   return request.delete(remove(id));
