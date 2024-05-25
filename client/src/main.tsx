d3c797decd76170e1520906e55a58ffcc9ccc020
Lnola <nola.luka@gmail.com>
2024-05-25 22:31:15 +0200

    :sparkles: Register the global toast component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,10 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { router } from './router.tsx';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,5 +15,17 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <RouterProvider router={router} />
     </LocalizationProvider>
+
+    <ToastContainer
+      position="bottom-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   </React.StrictMode>,
 );
