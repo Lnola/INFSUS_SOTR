4893fd7ccca21eb2c9c13bc9ea684aae2fdd59f7
Lnola <nola.luka@gmail.com>
2024-05-25 18:41:45 +0200

    :truck: Move drivers list to a separate component

---
+++ client/src/components/driver/DriverList.tsx
@@ -0,0 +1,39 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { driverUrls } from '@/api';
+import Error from '@/components/common/Error';
+import { StyledDataGridContainer } from '@/components/common/styled/StyledDataGridContainer';
+import usePagination from '@/hooks/usePagination';
+import Driver from '@/models/driver';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'contactNumber', headerName: 'Contact Number', width: 130 },
+  { field: 'employmentStartDate', headerName: 'Employment Start Date', width: 130, type: 'string' },
+  { field: 'employmentEndDate', headerName: 'Employment End Date', width: 130, type: 'string' },
+];
+
+const DriverList = () => {
+  const { data, count, isLoading, error, paginationModel, setPaginationModel } = usePagination<Driver[]>(
+    driverUrls.getAll,
+  );
+
+  if (error) return <Error error={error || 'Missing data'} />;
+  return (
+    <StyledDataGridContainer>
+      <DataGrid
+        columns={columns}
+        rows={data || []}
+        rowCount={count}
+        loading={isLoading}
+        pageSizeOptions={[1, 5, 10]}
+        paginationMode="server"
+        paginationModel={paginationModel}
+        onPaginationModelChange={setPaginationModel}
+      />
+    </StyledDataGridContainer>
+  );
+};
+
+export default DriverList;
--- client/src/pages/driver.tsx
+++ client/src/pages/driver.tsx
@@ -1,39 +1,11 @@
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { driverUrls } from '@/api';
-import Error from '@/components/common/Error';
-import { StyledDataGridContainer } from '@/components/common/styled/StyledDataGridContainer';
-import usePagination from '@/hooks/usePagination';
-import Driver from '@/models/driver';
+import DriverList from '@/components/driver/DriverList';
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  { field: 'contactNumber', headerName: 'Contact Number', width: 130 },
-  { field: 'employmentStartDate', headerName: 'Employment Start Date', width: 130, type: 'string' },
-  { field: 'employmentEndDate', headerName: 'Employment End Date', width: 130, type: 'string' },
-];
-
-const DriverList = () => {
-  const { data, count, isLoading, error, paginationModel, setPaginationModel } = usePagination<Driver[]>(
-    driverUrls.getAll,
-  );
-
-  if (error) return <Error error={error || 'Missing data'} />;
+const Driver = () => {
   return (
-    <StyledDataGridContainer>
-      <DataGrid
-        columns={columns}
-        rows={data || []}
-        rowCount={count}
-        loading={isLoading}
-        pageSizeOptions={[1, 5, 10]}
-        paginationMode="server"
-        paginationModel={paginationModel}
-        onPaginationModelChange={setPaginationModel}
-      />
-    </StyledDataGridContainer>
+    <>
+      <DriverList />
+    </>
   );
 };
 
-export default DriverList;
+export default Driver;
