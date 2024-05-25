b18c4e949f76eede163937f0073674d09ac48be8
Lnola <nola.luka@gmail.com>
2024-05-25 19:25:03 +0200

    :sparkles: Create a generic form dialog component

---
+++ client/src/components/common/FormDialog.tsx
@@ -0,0 +1,39 @@
+import { FormControl } from '@mui/material';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+type Props = {
+  title: string;
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  children: React.ReactNode;
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+};
+
+const FormDialog = ({ title, isOpen, setIsOpen, children, handleSubmit }: Props) => {
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Dialog open={isOpen} onClose={handleClose} fullWidth>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <form onSubmit={handleSubmit} id="dialog-form">
+          <FormControl fullWidth>{children}</FormControl>
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+        <Button type="submit" form="dialog-form">
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default FormDialog;
