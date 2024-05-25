d27f1aac977fc38f754b6f8b3d9f595329b9a267
Lnola <nola.luka@gmail.com>
2024-05-26 00:36:30 +0200

    :sparkles: Create the removeDriver api method

--- client/src/api/driver.ts
+++ client/src/api/driver.ts
@@ -1,7 +1,13 @@
 import urljoin from 'url-join';
+import request from './request';
 
 const base = urljoin('/api/drivers');
 const get = base;
 const create = base;
+const remove = (id: number) => urljoin(base, id.toString());
 
-export default { get, create };
+export default { get, create, remove };
+
+export const removeDriver = async (id: number) => {
+  return request.delete(remove(id));
+};
