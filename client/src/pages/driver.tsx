5438fb0e6c1149517516ea929b2687c10a2ada72
Lnola <nola.luka@gmail.com>
2024-05-25 19:25:37 +0200

    :sparkles: Add the add dialog to the main page

--- client/src/pages/driver.tsx
+++ client/src/pages/driver.tsx
@@ -1,9 +1,18 @@
+import { Button } from '@mui/material';
+import { useState } from 'react';
+import DriverAdd from '@/components/driver/DriverAdd';
 import DriverList from '@/components/driver/DriverList';
 
 const Driver = () => {
+  const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
+
   return (
     <>
+      <Button variant="contained" onClick={() => setIsAddDialogOpen(true)}>
+        Add Driver
+      </Button>
       <DriverList />
+      {isAddDialogOpen && <DriverAdd isOpen={isAddDialogOpen} setIsOpen={setIsAddDialogOpen} />}
     </>
   );
 };
