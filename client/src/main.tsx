ede7b095e978805646655957156ab52f04ccf565
Lnola <nola.luka@gmail.com>
2024-05-25 19:44:19 +0200

    :sparkles: Provide a date localization provider to the app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,6 @@
 import { CssBaseline } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
@@ -7,6 +9,8 @@ import { router } from './router.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CssBaseline />
-    <RouterProvider router={router} />
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <RouterProvider router={router} />
+    </LocalizationProvider>
   </React.StrictMode>,
 );
