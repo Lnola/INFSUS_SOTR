6a9aa98abb998b90e09c20a59e7ae4e85c6028ab
Lnola <nola.luka@gmail.com>
2024-05-25 19:44:31 +0200

    :sparkles: Add the missing form inputs

--- client/src/components/driver/DriverAdd.tsx
+++ client/src/components/driver/DriverAdd.tsx
@@ -1,6 +1,21 @@
+import styled from '@emotion/styled';
 import { TextField } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers';
 import { useState } from 'react';
-import FormDialog from '../common/FormDialog';
+import FormDialog from '@/components/common/FormDialog';
+
+const StyledDatePickerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  gap: 1rem;
+  margin-top: 1rem;
+
+  & > * {
+    width: 100%;
+  }
+`;
 
 type Props = {
   isOpen: boolean;
@@ -8,7 +23,13 @@ type Props = {
 };
 
 const DriverAdd = ({ isOpen, setIsOpen }: Props) => {
-  const [formData, setFormData] = useState({ firstName: '', lastName: '' });
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    contactNumber: '',
+    employmentStartDate: null,
+    employmentEndDate: null,
+  });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const name = event.target.name;
@@ -38,6 +59,28 @@ const DriverAdd = ({ isOpen, setIsOpen }: Props) => {
         value={formData.lastName}
         onChange={handleChange}
       />
+      <TextField
+        name="contactNumber"
+        label="Contact Number"
+        variant="standard"
+        type="number"
+        value={formData.contactNumber}
+        onChange={handleChange}
+      />
+      <StyledDatePickerContainer>
+        <DatePicker
+          name="employmentStartDate"
+          label="Employment Start Date"
+          value={formData.employmentStartDate}
+          onChange={newValue => setFormData(prev => ({ ...prev, employmentStartDate: newValue }))}
+        />
+        <DatePicker
+          name="employmentEndDate"
+          label="Employment End Date"
+          value={formData.employmentEndDate}
+          onChange={newValue => setFormData(prev => ({ ...prev, employmentEndDate: newValue }))}
+        />
+      </StyledDatePickerContainer>
     </FormDialog>
   );
 };
