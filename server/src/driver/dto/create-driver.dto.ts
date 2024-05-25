2cd76be6a56927fe6ce092a72dc99d24fc15a072
Lnola <nola.luka@gmail.com>
2024-05-25 23:18:37 +0200

    🦺 Validate not empty

--- server/src/driver/dto/create-driver.dto.ts
+++ server/src/driver/dto/create-driver.dto.ts
@@ -1,16 +1,21 @@
-import { IsDateString, IsPhoneNumber } from 'class-validator';
+import { IsDateString, IsNotEmpty, IsOptional, IsPhoneNumber } from 'class-validator';
 
 export class CreateDriverDto {
+  @IsNotEmpty()
   firstName: string;
 
+  @IsNotEmpty()
   lastName: string;
 
+  @IsNotEmpty()
   @IsPhoneNumber('US')
   contactNumber: string;
 
+  @IsNotEmpty()
   @IsDateString()
   employmentStartDate: Date;
 
+  @IsOptional()
   @IsDateString()
   employmentEndDate?: Date;
 }
