ba5fb83c092f4fb641d54482ad09219adf35ca50
Lnola <nola.luka@gmail.com>
2024-05-26 00:59:50 +0200

    :fire: Remove usePost

--- client/src/hooks/usePost.ts
+++
@@ -1,5 +0,0 @@
-import useFetch, { BaseProps } from './useFetch';
-
-export function usePost<T>({ path, params, start }: BaseProps) {
-  return useFetch<T>({ method: 'POST', path, params, start });
-}
