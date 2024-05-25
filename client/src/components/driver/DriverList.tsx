2a540d29a6d582e390be910fef3679df138b0a93
Lnola <nola.luka@gmail.com>
2024-05-26 00:06:12 +0200

    :sparkles: Add the edit and delete buttons

--- client/src/components/driver/DriverList.tsx
+++ client/src/components/driver/DriverList.tsx
@@ -1,29 +1,59 @@
+import { Button } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import Error from '@/components/common/Error';
 import { StyledDataGridContainer } from '@/components/common/styled/StyledDataGridContainer';
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  { field: 'contactNumber', headerName: 'Contact Number', width: 130 },
-  {
-    field: 'employmentStartDate',
-    headerName: 'Employment Start Date',
-    width: 130,
-    type: 'date',
-    valueFormatter: value => new Date(value).toLocaleDateString(),
-  },
-  {
-    field: 'employmentEndDate',
-    headerName: 'Employment End Date',
-    width: 130,
-    type: 'date',
-    valueFormatter: value => (value ? new Date(value).toLocaleDateString() : null),
-  },
-];
+const DriverList = ({
+  error,
+  data,
+  count,
+  isLoading,
+  paginationModel,
+  setPaginationModel,
+  handleEdit,
+  handleDelete,
+}) => {
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    { field: 'contactNumber', headerName: 'Contact Number', width: 130 },
+    {
+      field: 'employmentStartDate',
+      headerName: 'Employment Start Date',
+      width: 130,
+      type: 'date',
+      valueFormatter: value => new Date(value).toLocaleDateString(),
+    },
+    {
+      field: 'employmentEndDate',
+      headerName: 'Employment End Date',
+      width: 130,
+      type: 'date',
+      valueFormatter: value => (value ? new Date(value).toLocaleDateString() : null),
+    },
+    {
+      field: 'edit',
+      headerName: 'Edit',
+      width: 120,
+      renderCell: params => (
+        <Button variant="contained" onClick={handleEdit(params.row.id)}>
+          Edit
+        </Button>
+      ),
+    },
+    {
+      field: 'delete',
+      headerName: 'Delete',
+      width: 120,
+      renderCell: params => (
+        <Button variant="outlined" color="error" onClick={handleDelete(params.row.id)}>
+          Delete
+        </Button>
+      ),
+    },
+  ];
 
-const DriverList = ({ error, data, count, isLoading, paginationModel, setPaginationModel }) => {
   if (error) return <Error error={error || 'Missing data'} />;
   return (
     <StyledDataGridContainer>
