bca6382275a9a24d3554b7f0ca2d832a81f76b30
Lnola <nola.luka@gmail.com>
2024-05-26 04:53:55 +0200

    :sparkles: Register the missing entities

--- server/src/order/order.module.ts
+++ server/src/order/order.module.ts
@@ -1,12 +1,15 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
-import { OrderController } from './driver.controller';
-import { OrderService } from './driver.service';
+import Company from 'company/entities/company.entity';
+import Trailer from 'trailer/entities/trailer.entity';
+import Truck from 'truck/entities/truck.entity';
 import OrderStatus from './entities/order-status.entity';
 import Order from './entities/order.entity';
+import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Order, OrderStatus])],
+  imports: [MikroOrmModule.forFeature([Order, OrderStatus, Truck, Trailer, Company])],
   providers: [OrderService],
   controllers: [OrderController],
 })
