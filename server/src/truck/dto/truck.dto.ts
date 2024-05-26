4f3248d9917f9276a7686302916939ddfe07657a
Lnola <nola.luka@gmail.com>
2024-05-26 02:25:59 +0200

    :safety_vest: Create truck dto validation

--- server/src/truck/dto/truck.dto.ts
+++ server/src/truck/dto/truck.dto.ts
@@ -1,6 +1,12 @@
-export class TruckDto {
-  registration: string;
-  productionYear: string;
+import { IsNotEmpty, IsNumberString, IsOptional } from 'class-validator';
+import { TransportVehicleDto } from 'shared/models/transport-vehicle/transport-vehicle.dto';
+
+export class TruckDto extends TransportVehicleDto {
+  @IsNotEmpty()
+  @IsNumberString()
   tankCapacity: number;
+
+  @IsOptional()
+  @IsNumberString()
   horsepower: number;
 }
