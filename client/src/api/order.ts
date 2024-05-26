f68701ba3c009a17c257ab69778caec5d4ba006b
Lnola <nola.luka@gmail.com>
2024-05-26 04:15:41 +0200

    :sparkles: Add the missing api methods

--- client/src/api/order.ts
+++ client/src/api/order.ts
@@ -1,6 +1,22 @@
+import urljoin from 'url-join';
+import request from './request';
+
 export const urls = {
   base: '/api/orders',
   get() {
     return this.base;
   },
+  create() {
+    return this.base;
+  },
+  update(id: number) {
+    return urljoin(this.base, id.toString());
+  },
+  remove(id: number) {
+    return urljoin(this.base, id.toString());
+  },
+};
+
+export const removeOrder = (id: number) => {
+  return request.delete(urls.remove(id));
 };
