d830c4fffb59dbfe27ef09a4c211ace2c737a1aa
Lnola <nola.luka@gmail.com>
2024-05-26 20:53:14 +0200

    :bug: Catch the unique exception

--- server/src/driver/driver.service.ts
+++ server/src/driver/driver.service.ts
@@ -40,14 +40,21 @@ export class DriverService {
   }
 
   async update(id: number, updateDriverDto: CreateDriverDto) {
-    const driver = await this.driverRepository.findOne(id);
-    if (!driver) throw new NotFoundException('Driver not found!');
-    driver.firstName = updateDriverDto.firstName;
-    driver.lastName = updateDriverDto.lastName;
-    driver.contactNumber = updateDriverDto.contactNumber;
-    driver.employmentStartDate = updateDriverDto.employmentStartDate;
-    driver.employmentEndDate = updateDriverDto.employmentEndDate;
-    return await this.driverRepository.getEntityManager().persistAndFlush(driver);
+    try {
+      const driver = await this.driverRepository.findOne(id);
+      if (!driver) throw new NotFoundException('Driver not found!');
+      driver.firstName = updateDriverDto.firstName;
+      driver.lastName = updateDriverDto.lastName;
+      driver.contactNumber = updateDriverDto.contactNumber;
+      driver.employmentStartDate = updateDriverDto.employmentStartDate;
+      driver.employmentEndDate = updateDriverDto.employmentEndDate;
+      return await this.driverRepository.getEntityManager().persistAndFlush(driver);
+    } catch (error) {
+      if (error instanceof UniqueConstraintViolationException)
+        throw new BadRequestException('Driver with this contact number already exists!');
+
+      throw error;
+    }
   }
 
   async delete(id: number) {
